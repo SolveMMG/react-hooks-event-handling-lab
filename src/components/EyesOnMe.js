fdbc74f3ae5970ea5f56b30919fc573a2e5e9918
Your Name <mikachupichu@gmail.com>
2023-11-16 12:36:27 +0300

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+
+  function Focus(){
+    console.log('Good!');
+  }
+
+  function Blur(){
+    console.log('Hey! Eyes on me!');
+  }
+    
+  return (
+    <div >
+        <button
+        onFocus={Focus}
+        onBlur={Blur}
+        >Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function Consoling(){
+        console.log("Entering password...");
+    }
+
     return (
-        <div></div>
+        <div>
+            <input 
+            type="password"
+            name="password"
+            onChange={Consoling}
+            />
+        </div>
     )
 }
 
